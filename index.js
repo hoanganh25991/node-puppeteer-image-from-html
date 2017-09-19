9c39fa9cfff4bfde745d9c59f3d4c74267c5373f
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-09-19 11:06:47 +0700

    Create image with puppeteer

--- index.js
+++ index.js
@@ -3,23 +3,37 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  // await page.goto('https://example.com');
-  const dimensions = await page.evaluate((selector) => {
-  	console.log(selector);
-  	// let bill = document.querySelector('#bill');
-   //  return {
-   //    width: bill.clientWidth,
-   //    height: bill.clientHeight,
-   //    deviceScaleFactor: window.devicePixelRatio
-   //  };
-  });
-  page
-    .waitForSelector('img')
-    .then(() => console.log('First URL with image: ' + currentURL));
-  console.log(dimensions);
-  await page.setViewport({width: 576, height: 500});
   await page.goto('file:///D:/work-station/node-puppeteer/receipt-html/receipt.html');
-  await page.screenshot({path: 'example.png'});
+  let dimensions  = await page.evaluate(() => {
+  	let bill = document.querySelector('#bill');
+  	return {width: bill.clientWidth, height: bill.clientHeight};
+  });
+  console.log(dimensions);
+  // page.waitForSelector('#bill').then(bill => console.log(bill));
+  await page.setViewport(dimensions);
+  await page.screenshot({path: 'receipt.png'});
 
   browser.close();
 })();
+
+
+// const { Chromeless } = require('chromeless')
+
+// async function run() {
+//   const chromeless = new Chromeless()
+
+//   await chromeless
+//     .goto('file:///D:/work-station/node-puppeteer/receipt-html/receipt.html')
+
+//   const dimesions = await chromeless.evaluate(() => {
+//   	let bill = document.querySelector('#bill');
+//     return {width: bill.clientWidth}
+//   })
+//   console.log(dimesions);
+//   await chromeless.setViewport({width: 576, height: 500})
+//   await screenshot = chromeless.screenshot();
+//   console.log(screenshot)
+//   await chromeless.end()
+// }
+
+// run().catch(console.error.bind(console))
